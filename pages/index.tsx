69a5aa656114d40e5d76c48110cfbf5e109f35c6
Web Time <Web Time>
2022-12-31 13:45:40 +0200

    implemented getgetStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,27 @@
-import Banner from '../src/components/Banner/Banner'
-import Users from '../src/components/Users/Users'
-import React, { useEffect, useState } from 'react';
-import endpoints from '../src/api/api';
+import Banner        from '../src/components/Banner/Banner'
+import Users         from '../src/components/Users/Users'
 import { DataProps } from '../src/interfaces';
+import { FC }        from 'react';
 
 
-export default function Home() {
 
-	const [ data, setData ] = useState<DataProps | any>({})
-
-	useEffect(() => {
-		const getItems = async () => {
-			const getData = await endpoints.getData()
-			setData(getData.data)
-		}
-
-		getItems()
-
-	}, [])
-
+const Home:FC<DataProps | any> = ({data}) => {
 	return (
 		<>
 			<Banner banner={ data.video }/>
 			<Users users={ data.users }/>
 		</>
 	)
+}
+
+export default Home
+
+export async function getStaticProps() {
+	const res = await fetch('https://tz.smart-ui.pro/');
+	const response = await res.json()
+	return {
+		props:{
+			data:response,
+		}
+	}
 }
